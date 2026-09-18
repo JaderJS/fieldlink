29ebc72556f552b880b44b5f562926d0ebf135eb
JaderJS <jader.jader55@gmail.com>
2026-09-18 18:50:37 -0400

    changes in front

--- apps/front/src/lib/auth.ts
+++ apps/front/src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/client";
+import { clientEnv } from "@/env/client";
 
 export const authClient = createAuthClient({
 	basePath: "/auth",
-	baseURL: import.meta.env.VITE_AUTH_URL,
+	baseURL: clientEnv.VITE_API_URL,
 });
